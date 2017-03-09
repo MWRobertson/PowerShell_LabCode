69384f05a2a8550e6dd445e098ecc74cace54d9b
Mike Robertson <Mike Robertson>
2017-03-09 16:09:03 -0500

    Added xPSDesiredStateConfiguration to push prereqs

--- PushDSC_Configs/PushDSC_Prereqs.ps1
+++ PushDSC_Configs/PushDSC_Prereqs.ps1
@@ -1,4 +1,5 @@
 Install-PackageProvider NuGet -Force
 
+Install-Module -Name xPSDesiredStateConfiguration -Force -Verbose
 Install-Module -Name xSmbShare -Force -Verbose
 Install-Module -Name xTimeZone -Force -Verbose
